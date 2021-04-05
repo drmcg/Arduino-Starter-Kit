662f0c716c40924a189b8313667281a63838e3a4
Duncan McGregor <Duncan.McGregor@outlook.com>
2021-04-05 17:15:52 +1200

    adding buttons and fan app

---
+++ 3 Using Buttons/buttons1.ino
@@ -0,0 +1,23 @@
+						// Setting variables which can be easily called to later
+  int Button = 12;				// Input pin from the button
+  int Buzzer = 11;				// Output pin to the buzzer
+  int Val = 0;					// A variable to store the button value
+
+void setup()					// Runs once when sketch starts
+						// Setting the pin type & defining the I/O
+{
+  pinMode(Button, INPUT_PULLUP);		// Setting the button pin as an input which
+						// uses an internal pullup resistor on the Uno board
+  pinMode(Buzzer, OUTPUT);			// Setting the buzzer pin as an output
+}
+
+void loop()					// Runs repeatedly
+{
+  Val = digitalRead(Button);			// Setting the Val variable to the output of the
+						// button, which can be either HIGH or LOW
+  if (Val == LOW) {				// Statement to determine the state of the button
+    digitalWrite(Buzzer, HIGH); 		// If the button is pressed, the buzzer will sound
+  } else {
+    digitalWrite(Buzzer, LOW); 			// Else, the buzzer will not sound
+  }
+}
